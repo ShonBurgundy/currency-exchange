7f6fad3c0dd02f112e35caa793a6cdc0d305680a
shonburgundy <seanbeloussow@yahoo.com>
2020-04-24 16:01:36 -0700

    add currencFrom functionality

--- src/exchange-rate.js
+++ src/exchange-rate.js
@@ -1,10 +1,10 @@
-// import $ from 'jquery';
-
+//Business Logic
 export class ExchangeRate {
   
-  async getExchangeRate() {
+  async getExchangeRate(currency1) {
     try {
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/FFF`);
+      console.log(currency1);
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${currency1}`);
       let jsonifiedResponse;
       if (response.ok && response.status === 200) {
         jsonifiedResponse = await response.json();
@@ -13,6 +13,7 @@ export class ExchangeRate {
       }
       return jsonifiedResponse;
     } catch(error) {
+      alert("Service is down, go outside, but maintain social distancing!")
       return false;
     }
   }
@@ -23,96 +24,3 @@ export class ExchangeRate {
   }
 }
  
-
-
-//   getIngredientsFromDrink (drinkArray) {
-//     let ingredients = [];
-//     $.each(drinkArray, function(key,value){
-//       if (key.startsWith("strIngredient") && value) {
-//         ingredients.push(value);
-//       }
-//     });
-//     return ingredients;
-//   }
-
-//   getDrinkList (response) {
-//     let drinkList = [];
-//     for (let i = 0; i <= (response.drinks.length - 1); i++){ 
-//       drinkList.push(response.drinks[i].strDrink);
-//     }
-//     return drinkList;
-//   }
-  
-//   getIngredientArray(response) {
-//     if (response) {
-//       //We should get the name of all the drinks and let user pick which drink they would like to see ingredients for via for loop?
-//       let drinkArray = response.drinks[0];
-//       let ingredients = this.getIngredientsFromDrink(drinkArray);
-//       return ingredients;
-//     }
-//   }
-// }
-
-
-// export class RandomCocktailService {
-//   async getRandomCocktailInfo() {
-//     try {
-//       let response = await fetch(`https://www.thecocktaildb.com/api/json/v1/${process.env.API_KEY}/random.php`);
-//       let jsonifiedResponse;
-//       if (response.ok && response.status === 200) {
-//         jsonifiedResponse = await response.json();
-//       } else {
-//         jsonifiedResponse = false;
-//       }
-//       return jsonifiedResponse;
-//     } catch(error) {
-//       return false;
-//     }
-//   }
-
-//   getRandomDrink (response) {
-//     let drink = response.drinks[0].strDrink;
-//     return drink;
-//   }
-
-//   getIngredientsFromDrink (drinkArray) {
-//     let ingredients = [];
-//     $.each(drinkArray, function(key,value){
-//       if (key.startsWith("strIngredient") && value) {
-//         ingredients.push(value);
-//       }
-//     });
-//     return ingredients;
-//   }
-
-//   getRandomIngredientArray(response) {
-//     if (response) {
-//       let drinkArray = response.drinks[0];
-//       let ingredients = this.getIngredientsFromDrink(drinkArray);
-//       return ingredients;
-//     }
-//   }
-// }
-
-// export class GiphyService {
-//   async giphyCall(drink) {
-//     try {
-//       let response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY_GIPHY}&q=${drink}`);
-//       let jsonifiedResponse;
-//       if (response.ok && response.status === 200) {
-//         jsonifiedResponse = await response.json();
-//       } else {
-//         jsonifiedResponse = false;
-//       }
-//       return jsonifiedResponse;
-//     } catch(error) {
-//       return false;
-//     } 
-//   }
-//   getGif(gifResponse) {
-//     if (gifResponse) {
-//       let gifUrl = gifResponse.data[0].images.downsized_large.url;
-//       return gifUrl;
-//     }
-//   }
-// }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 //UI
-// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -7,25 +6,32 @@ import { ExchangeRate } from './../src/exchange-rate.js';
 
 $(document).ready(function() {
   $("#currencyExchange").click(function() {
-    let currency = $("#currencySelector").val();
-    console.log(currency);
+    let currency1 = $("#currencyFrom").val();
+    if (currency1 === "null")
+      return $("#output").text("You have to pick a currency!");
+    let currency2 = $("#currencyTo").val();
+    if (currency2 === "null")
+      return $("#output").text("You have to pick a currency!");
     let dollarAmount = $("#currencyInput").val();
-    if (dollarAmount !== "") {
-      console.log(dollarAmount);
+    if (!dollarAmount) {
+      return $("#output").text("You must input currency amount, foo!");
+    }
       (async () => {
         let exchangeRate = new ExchangeRate();
-        const response = await exchangeRate.getExchangeRate();
-        if (response.error || response.status !== 200) {
-          alert 
+        const response = await exchangeRate.getExchangeRate(currency1);
+         if (response.error === "invalid-key") {
+          alert("You shall not pass! -Gandolf");
+        } else if (response.error) {
+          alert(response.error);
         }
-        let conversionRate = response.conversion_rates[currency];
+        let conversionRate = response.conversion_rates[currency2];
         const calculated = exchangeRate.calculateRate(conversionRate, dollarAmount);
-        $("#output").text(calculated + " " + currency);
-
+        $("#output").text(calculated + " " + currency2);
       })();
-    }
   });
 });
+
+
 
 // function displayIngredientList(ingredientArray) {
 //   let ingredientListDisplay = $("#showIngredientsList");
